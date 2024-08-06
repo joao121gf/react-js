a985ea3279a13108dd0caffdec0b798d7b3e253e
João Giovani <giovannirs957@gmail.com>
2024-08-06 18:13:51 -0300

    react files update

--- app01/app01/src/App.js
+++ app01/app01/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Eu from "./componentes/imgs/eu.jpeg";
+import Anuncio from "./componentes/imgs/anuncio.png";
 
 export default function App() {
+  const agency = "Web Stones";
+  const client = "Macedo Treinamentos";
+  const data = () => {
+    return "03/08/2024";
+  };
   return (
     <>
-      <h1>Hello everyone</h1>
-      <p></p>
-      <style>
-        
-      </style>
+      <h1>{"Bem vindo à " + agency}</h1>
+      <p>{"Muito prazer " + client}</p>
+      <p>{"Hoje é dia : " + data()}</p>
+      <img src={Eu} alt="Eu" />
+      <img src={Anuncio} alt="Anuncio " />
     </>
   );
 }
+
+
